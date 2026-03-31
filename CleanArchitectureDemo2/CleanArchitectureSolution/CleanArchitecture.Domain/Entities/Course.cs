d9295c16a455b503384c2e4241b32e528a9de60e
SlimBenRomdhane <taf.slim.benromdhane@gmail.com>
2026-03-31 02:56:52 +0200

    Add Course Domain Model

---
+++ CleanArchitectureDemo2/CleanArchitectureSolution/CleanArchitecture.Domain/Entities/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanArchitecture.Domain.Entities
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
